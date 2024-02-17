39d56d93cecec0f2e7ca7942f537af05683dd078
PNinitzWebsite <101594204+PNinitzWebsite@users.noreply.github.com>
2024-02-18 01:51:57 +0700

    Add files via upload

--- controllers/loginUserController.js
+++ controllers/loginUserController.js
@@ -16,16 +16,25 @@ module.exports = (req, res) => {
                 if (match && user._id == "65cf202d5c3b8db9dfdc7689") {
                     req.session.userId = user._id
                     res.redirect('/admin')
-                } 
-                if (match && user._id !== "65cf202d5c3b8db9dfdc7689") {
-                    req.session.userId = user._id
-                    res.redirect('/home')
                 }else {
-                    res.send('<script>alert("รหัสผ่านผิด กรุณากรอกใหม่!!"); window.location.href = "/login";</script>');
+                    if (user._id == "65cf202d5c3b8db9dfdc7689") {
+                        res.send('<script>alert("รหัสผิด กรุณากรอกใหม่!!"); window.location.href = "/login";</script>');
+                    }else {
+                        if(match && user._id !== "65cf202d5c3b8db9dfdc7689"){
+                            req.session.userId = user._id
+                            res.redirect('/home')
+                        }else{
+                            res.send('<script>alert("รหัสผิด กรุณากรอกใหม่!!"); window.location.href = "/login";</script>');
+                        }
+                        
+                    }
                 }
+                
+
+               
             })
         } else {
-            res.redirect('/login')
+            res.send('<script>alert("ไม่มีในระบบ กรุณากรอกใหม่!!"); window.location.href = "/login";</script>');
         }
     })
 }
